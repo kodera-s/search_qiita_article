ffadcf50cc82e01ae177fc87651e2434dcd4c344
s-kodera <s.m.k-do40@outlook.com>
2021-01-14 12:20:42 +0900

    add search file

--- search-qiita-article/src/store/index.ts
+++ search-qiita-article/src/store/index.ts
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    qiitaDataSet: {}
+  },
+  getters: {
+    getQiitaDataSet: (state) => state.qiitaDataSet
   },
   mutations: {
+    mutateQiitaApi(state, payload){
+      state.qiitaDataSet = payload
+    }
   },
   actions: {
-  },
-  modules: {
+    commitQiitaApi(store, payload){
+      return axios.get('https://qiita.com/api/v2/items', payload )
+      .then(response => {
+        store.commit('mutateQiitaApi', response.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    }
   }
 })
